840daba4a5dc8244adc661a85461238f07a3b107
chris wagner <cwagne17@students.towson.edu>
2023-11-11 17:52:43 -0500

    add local file save to tls private key

--- main.tf
+++ main.tf
@@ -91,6 +91,11 @@ resource "tls_private_key" "ssh" {
   rsa_bits  = 4096
 }
 
+resource "local_file" "private_key" {
+  filename = "${var.project_name}-private.pem"
+  content  = tls_private_key.ssh.private_key_pem
+}
+
 resource "aws_key_pair" "ssh" {
   key_name   = var.project_name
   public_key = tls_private_key.ssh.public_key_openssh
