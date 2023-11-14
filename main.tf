75c8bf19d4a9e127f8f96a57e6fc7beed146c0a1
Christopher Wagner <70287489+Cwagne17@users.noreply.github.com>
2023-11-14 15:22:07 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,9 +23,7 @@ locals {
   vpc_id    = local.create_vpc ? module.vpc.vpc_id : var.vpc_id
   subnet_id = local.create_vpc ? module.vpc.public_subnets[0] : var.subnet_id
 
-  create_ssh_key = var.ssh_public_key == ""
-
-  public_key = local.create_ssh_key ? tls_private_key.ssh[0].public_key_openssh : var.ssh_public_key
+  public_key = var.create_ssh_key ? tls_private_key.ssh[0].public_key_openssh : var.ssh_public_key
 }
 
 # -------------------------------------------
@@ -100,7 +98,7 @@ data "aws_ami" "al2023" {
 
 # create SSH key pair
 resource "tls_private_key" "ssh" {
-  count = local.create_ssh_key ? 1 : 0
+  count = var.create_ssh_key ? 1 : 0
 
   algorithm = "RSA"
   rsa_bits  = 4096
@@ -112,7 +110,7 @@ locals {
 }
 
 resource "local_file" "private_key" {
-  count = local.create_ssh_key ? 1 : 0
+  count = var.create_ssh_key ? 1 : 0
 
   filename = local.file_name
   content  = tls_private_key.ssh[0].private_key_pem
