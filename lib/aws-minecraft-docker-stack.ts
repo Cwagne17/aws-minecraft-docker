b5b035e322cbb3687e296aa372e24c560f78409b
Christopher Wagner <christopher.wagner@secured.team>
2025-04-12 17:08:50 -0400

    implement vpc and ecr bootstrap

--- lib/aws-minecraft-docker-stack.ts
+++ lib/aws-minecraft-docker-stack.ts
@@ -1,16 +1,88 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as aws_ec2 from "aws-cdk-lib/aws-ec2";
+import * as aws_ssm from "aws-cdk-lib/aws-ssm";
+import * as aws_ecr from "aws-cdk-lib/aws-ecr";
 
-export class AwsMinecraftDockerStack extends cdk.Stack {
+export class AwsMinecraftBootsrapStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Create a VPC to deploy the Minecraft infrastructure to
+    const vpc = new aws_ec2.Vpc(this, "rMinecraftVpc", {
+      vpcName: "MinecraftVpc",
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: "MinecraftPublicSubnet",
+          subnetType: aws_ec2.SubnetType.PUBLIC,
+        },
+      ],
+      natGateways: 0,
+      restrictDefaultSecurityGroup: true,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsMinecraftDockerQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Create an ECR repository to store the Minecraft Docker image
+    const repository = new aws_ecr.Repository(this, "rMinecraftRepository", {
+      repositoryName: "minecraft-server",
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      emptyOnDelete: true,
+    });
+
+    // Save bootstrap information to SSM Parameter Store
+    new aws_ssm.StringParameter(this, "rVpcIdParameter", {
+      parameterName: `${this.stackName}/VpcId`,
+      stringValue: vpc.vpcId,
+      description: "VPC ID of the Minecraft VPC",
+    });
+
+    new aws_ssm.StringListParameter(this, "rPublicSubnetIdsParameter", {
+      parameterName: `${this.stackName}/PublicSubnetIds`,
+      stringListValue: vpc.publicSubnets.map((subnet) => subnet.subnetId),
+      description: "Public Subnet IDs of the Minecraft VPC",
+    });
+
+    new aws_ssm.StringParameter(this, "rRepositoryNameParameter", {
+      parameterName: `${this.stackName}/RepositoryName`,
+      stringValue: repository.repositoryName,
+      description: "ECR Repository Name for the Minecraft Docker image",
+    });
+
+    new aws_ssm.StringParameter(this, "rRepositoryUriParameter", {
+      parameterName: `${this.stackName}/RepositoryUri`,
+      stringValue: repository.repositoryUri,
+      description: "ECR Repository URI for the Minecraft Docker image",
+    });
+
+    // Outputs for the CloudFormation stack
+    new cdk.CfnOutput(this, "oVpcId", {
+      value: vpc.vpcId,
+      description: "VPC ID",
+      exportName: "VpcId",
+    });
+
+    new cdk.CfnOutput(this, "oVpcCidr", {
+      value: vpc.vpcCidrBlock,
+      description: "VPC CIDR Block",
+      exportName: "VpcCidrBlock",
+    });
+
+    new cdk.CfnOutput(this, "oPublicSubnetIds", {
+      value: vpc.publicSubnets.map((subnet) => subnet.subnetId).join(","),
+      description: "Public Subnet IDs",
+      exportName: "PublicSubnetIds",
+    });
+
+    new cdk.CfnOutput(this, "oRepositoryName", {
+      value: repository.repositoryName,
+      description: "ECR Repository Name",
+      exportName: "RepositoryName",
+    });
+
+    new cdk.CfnOutput(this, "oRepositoryUri", {
+      value: repository.repositoryUri,
+      description: "ECR Repository URI",
+      exportName: "RepositoryUri",
+    });
   }
 }
