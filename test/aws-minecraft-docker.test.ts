b1f5fb27a3094cb0fb928d1394725b072a3bf40d
Christopher Wagner <christopher.wagner@secured.team>
2025-04-12 16:37:34 -0400

    reorganzie repo

--- test/aws-minecraft-docker.test.ts
+++ test/aws-minecraft-docker.test.ts
@@ -1,13 +1,13 @@
 // import * as cdk from 'aws-cdk-lib';
 // import { Template } from 'aws-cdk-lib/assertions';
-// import * as Minecraft from '../lib/minecraft-stack';
+// import * as AwsMinecraftDocker from '../lib/aws-minecraft-docker-stack';
 
 // example test. To run these tests, uncomment this file along with the
-// example resource in lib/minecraft-stack.ts
+// example resource in lib/aws-minecraft-docker-stack.ts
 test('SQS Queue Created', () => {
 //   const app = new cdk.App();
 //     // WHEN
-//   const stack = new Minecraft.MinecraftStack(app, 'MyTestStack');
+//   const stack = new AwsMinecraftDocker.AwsMinecraftDockerStack(app, 'MyTestStack');
 //     // THEN
 //   const template = Template.fromStack(stack);
 
